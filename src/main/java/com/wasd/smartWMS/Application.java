b43f8f742ac92b05d37dca3c76ec86097760cdcc
Barkyeongchan <xodudcksxo@gmail.com>
2025-09-23 17:41:47 +0900

    경로 이름 리팩토링 / park -> wasd, springboot -> smartWMS

--- src/main/java/com/wasd/smartWMS/Application.java
+++ src/main/java/com/wasd/smartWMS/Application.java
@@ -1,8 +1,7 @@
-package com.park.springboot;
+package com.wasd.smartWMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
