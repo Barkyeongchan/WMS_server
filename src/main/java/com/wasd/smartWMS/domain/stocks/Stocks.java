6729e4f667f7d7f4bdbdc0f7f8988529f70172bc
Barkyeongchan <xodudcksxo@gmail.com>
2025-09-25 10:33:28 +0900

    주석 추가

--- src/main/java/com/wasd/smartWMS/domain/stocks/Stocks.java
+++ src/main/java/com/wasd/smartWMS/domain/stocks/Stocks.java
@@ -1,3 +1,4 @@
+// 테이블 구조를 정의 함
 package com.wasd.smartWMS.domain.stocks;
 
 import com.wasd.smartWMS.domain.BaseTimeEntity;
@@ -9,26 +10,26 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-@Table(name = "Stocks")
+@Table(name = "Stocks")    // DB 테이블 이름
 public class Stocks extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 500, nullable = false)
+    @Column(length = 100, nullable = false)    // length : 글자수 제한 (100자까지), nullable : 비어있으면 오류
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false)    // 속성의 자료형이 Integer기 때문에 length는 쓰지않음
     private Integer inventory;
 
     @Builder
-    public Stocks(String name, Integer inventory) {
+    public Stocks(String name, Integer inventory) {    // 속성의 값을 생성
         this.name = name;
         this.inventory = inventory;
     }
 
-    public void Update(String name, Integer inventory) {
+    public void Update(String name, Integer inventory) {    // 속성의 값을 업데이트
         this.name = name;
         this.inventory = inventory;
     }
--- src/main/java/com/wasd/smartWMS/domain/stocks/StocksRepository.java
+++ src/main/java/com/wasd/smartWMS/domain/stocks/StocksRepository.java
@@ -1,3 +1,4 @@
+// 실제 DB에서 CRUD(create, read, update, delete)를 수행함
 package com.wasd.smartWMS.domain.stocks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,6 @@ import java.util.List;
 
 public interface StocksRepository extends JpaRepository<Stocks, Long> {
 
-    @Query("SELECT r FROM Stocks r ORDER BY r.id DSEC")
-    List<Stocks> findAllDsec();
+    @Query("SELECT r FROM Stocks r ORDER BY r.id DESC")
+    List<Stocks> findAllDesc();
 }
