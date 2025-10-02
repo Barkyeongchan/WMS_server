5e42f7f5b6633e68c23cac7d9438fe55828fb2d2
Barkyeongchan <xodudcksxo@gmail.com>
2025-10-02 14:44:21 +0900

    admins 테이블의 구조 정의

--- src/main/java/com/wasd/smartWMS/domain/admins/Admins.java
+++ src/main/java/com/wasd/smartWMS/domain/admins/Admins.java
@@ -1,35 +1,40 @@
-// Admins 테이블 구조 정의
 package com.wasd.smartWMS.domain.admins;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+@Entity
+@Table(name = "admins")
 @Getter
 @NoArgsConstructor
-@Entity
-@Table(name="Admins")
-
 public class Admins {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // PK 자동 생성
 
-    @Column(nullable = false)
-    private String name;
+    @Column(nullable = false, unique = true) // 빈값 허용 안함, 중복 허용 안함
+    private String username;
+
+    @Column(nullable = false) // DB와 일치
+    private String password;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)    // Enum를 문자열로 저장, 더 깔끔하고 안전함
     private Role role;  // 관리자 등급 - MAIN, SUB
 
-    private Long mainAdminId;  // SUB 관리자가 속해있는 MAIN 관리자 ID(PK)
+    @CreationTimestamp // DB에 INSERT될 때 자동으로 현재 시간 저장
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 
     @Builder
-    public Admins(String name, Role role, Long mainAdminId) {
-        this.name = name;
+    public Admins(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
         this.role = role;
-        this.mainAdminId = mainAdminId;
     }
 }
