ec0f0067291b3cda314e31a4f51180b7924abb8d
Barkyeongchan <xodudcksxo@gmail.com>
2025-10-02 15:45:24 +0900

    @Setter 추가

--- src/main/java/com/wasd/smartWMS/domain/admins/Admins.java
+++ src/main/java/com/wasd/smartWMS/domain/admins/Admins.java
@@ -5,10 +5,12 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "admins")
+@Setter
 @Getter
 @NoArgsConstructor
 public class Admins {
