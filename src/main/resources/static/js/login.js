3a2072316b492d46e2565b514f8b40eb586d5c73
Barkyeongchan <xodudcksxo@gmail.com>
2025-10-13 16:15:16 +0900

    메세지 추가

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // 회원가입 버튼 클릭
+document.addEventListener("DOMContentLoaded", () => {
     const signupBtn = document.getElementById("signupBtn");
-    signupBtn.addEventListener("click", function () {
-        // /signup 페이지로 이동
-        window.location.href = "/signup";
-    });
+
+    // 회원가입 버튼 클릭 이벤트
+    if (signupBtn) {
+        signupBtn.addEventListener("click", () => {
+            const showSignup = signupBtn.dataset.showSignup || "false";
+            if (showSignup.toLowerCase() === 'true') {
+                window.location.href = "/signup";
+            } else {
+                alert("가입은 관리자에게 문의하세요");
+            }
+        });
+    }
+
+    // 회원가입 완료 후 로그인 화면에서 메시지 띄우기
+    const signupSuccess = "{{signupSuccess}}" || "false";
+    if (signupSuccess.toLowerCase() === "true") {
+        alert("회원가입이 완료되었습니다!");
+        window.location.href = "/login";
+    }
 });
